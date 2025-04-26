9c912a1c2d1854336da0c3dc8d579296492b7904
vvyre <seung.yoon.yu@gmail.com>
2025-04-26 17:12:51 +0900

    타이틀바 컴포넌트 추가

---
+++ src/components/layouts/electron/titlebar.tsx
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react'
+import { isBrowser } from 'utils/fn/runtime'
+
+export function ElectronTitleBar() {
+  if (isBrowser()) return null
+  return (
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      <div
+        css={css`
+          -webkit-app-region: drag;
+          display: flex;
+          height: 2.5rem;
+          line-height: 0;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+        `}
+      >
+        <span
+          css={css`
+            font-size: 0.85rem;
+            font-weight: 500;
+          `}
+        >
+          챗팅
+        </span>
+      </div>
+    </div>
+  )
+}
