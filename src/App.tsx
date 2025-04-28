114b047ce1f05da7b7816304040ea3b77f17beef
vvyre <seung.yoon.yu@gmail.com>
2025-04-28 15:06:36 +0900

    글로벌 테마 변수 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { Routes } from 'pages/routes'
-import { PageLayout } from 'pages/layout'
 import { Global } from '@emotion/react'
 import { DevTools } from 'jotai-devtools'
 import { Providers } from 'components/providers'
 import { globalStyles } from 'styles/global-style'
+import { GlobalThemeVars } from 'styles/global-theme-vars'
 
 function App() {
   return (
     <Providers>
       <DevTools position="top-right" />
+      <GlobalThemeVars />
       <Global styles={globalStyles} />
-      <PageLayout>
-        <Routes />
-      </PageLayout>
+      <Routes />
     </Providers>
   )
 }
